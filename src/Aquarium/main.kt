363ec1d24d47e01b8e824ec7520e38316951ad16
Soojeong <soojeongshin2@gmail.com>
2019-05-28 16:44:22 +0900

    feat: Define Classes

---
+++ src/Aquarium/main.kt
@@ -0,0 +1,21 @@
+package Aquarium
+
+fun main (args: Array<String>) {
+    buildAquarium()
+}
+
+fun buildAquarium() {
+
+    val myAquarium = Aquarium()
+
+    println("Length: ${myAquarium.length} " +
+            "Width: ${myAquarium.width} " +
+            "Height: ${myAquarium.height}")
+
+    myAquarium.height = 80
+
+    println("Height: ${myAquarium.height} cm")
+
+    println("Volume: ${myAquarium.volume} liters")
+}
+
